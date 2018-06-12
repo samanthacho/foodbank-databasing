c3f48d182c2bf9b4572331de4b2f0d3e56969389
Kiki Hsieh <kikivancouver@yahoo.com>
2018-06-12 16:03:38 -0700

    updated queries, need a TA to check

--- queries.sql
+++ queries.sql
@@ -5,17 +5,41 @@ SELECT username
 FROM money_collect
 WHERE username=uname
 
+// Adding Employees
+INSERT into employee
+VALUES(a,b,c,e);
+
+//Updating Employees
+UPDATE employee
+SET name = b
+WHERE name = a
+
+// Employee report
+SELECT dname, amount
+FROM money_collect
+WHERE username = uname
+
+//
+SELECT name, item
+FROM item_collects
+WHERE username=unmame
+
 // Staff list: args; NONE
 SELECT name, phone
 FROM employee
 
-// Donation list: args; NONE
-SELECT dname, date, amount
-FROM money_collect
+// Donation list: args; NONE  
+SELECT dname, moneydate, amount
+FROM money_collect 
+
+//
+SELECT name, itemdate, item
+FROM item_collects
 
 // Purchase list: args; NONE
 SELECT item, pamount
 FROM purchase_make
+
 
 // Making purchase: args(item, amount)
 (SELECT sum(amount) FROM money_collect GROUP BY amount) -
@@ -25,3 +49,78 @@ amount -
 // Amount Available: args; NONE
 (SELECT sum(amount) FROM money_collect GROUP BY amount) -
 (SELECT sum(pamount) FROM purchase_make GROUP BY pamount)
+
+
+
+//COLLECTION SCREEN
+
+//Item Lookup
+SELECT name
+FROM item
+WHERE name = instItem/*not sure what to put here*/
+
+INSERT into money_collect 
+VALUES (a,b,c,d,e,f,g);
+
+SELECT name
+FROM item
+WHERE name= IDItem
+
+SELECT location
+FROM item
+WHERE name =itemID;
+
+INSERT into item_collects 
+VALUES (a,b,c,d,e,f)
+
+SELECT username
+FROM ADMIN
+WHERE username=uname
+
+INSERT into item
+VALUES (a,b,c)
+
+SELECT exDate
+FROM expirationDate
+WHERE exDate =
+
+INSERT expirationDate 
+VALUES ()
+
+// LOGIN SCREEN
+
+// Login
+SELECT password
+FROM employee 
+WHERE username= uname
+
+SELECT username
+FROM admin
+WHERE username=uname
+
+
+// VOLUNTEER SCREEN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
