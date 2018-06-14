9b7377833879b533e992055023800daea6567202
Kiki Hsieh <kikivancouver@yahoo.com>
2018-06-13 22:47:21 -0700

    updated db

--- database.sql
+++ database.sql
@@ -1,73 +1,89 @@
-set FOREIGN_KEY_CHECKS=0 ;
+set FOREIGN_KEY_CHECKS=0;
+-- ERROR ; unique/primary keys in table referenced by foreign keys
+drop table item_Table; 
+drop table expirationDate;
+drop table employee;
+
+drop table expiresOn;
+
+-- ERROR: table/view doesnt exist
 drop table money_collects;
 drop table item_collects;
-drop table item_is;
-drop table item_distributes;
-drop table group;
-drop table has;
-drop table collection;
-drop table distribution;
-drop table adds;
-drop table purchase_makes;
-drop table shift_logDelete;
-drop table works;
-drop table volunteer_add;
-drop table administrator;
-drop table employee;
-drop table expiresOn;
-drop table expirationDate;
-drop table shift;
-drop table item;
 
-create table item(
-	name varchar2(30) primary key,
+-- ERROR ; unique/primary keys in table referenced by foreign keys
+drop table food_g;
+
+drop table f_has;
+
+-- ERROR: table/view doesnt exist
+drop table collection_work;
+drop table distribution_work;
+
+
+drop table purchase_makes;
+drop table shift;
+
+-- ERROR: table/view doesnt exist
+drop table volunteer_add;
+
+-- ERROR ; unique/primary keys in table referenced by foreign keys
+drop table admin;
+
+
+create table item_Table(
+	-- ERROR: ORA-00955: name is already used by an existing object
+	name varchar2(30) not null primary key,
 	category varchar2(30),
 	location varchar2(30) not null
 );
 
-grant select on item to public;
+grant select on item_Table to public;
 
 create table expirationDate(
-	exDate date primary key
+	-- ERROR: ORA-00955: name is already used by an existing object
+	exDate number not null primary key
 );
 
 grant select on expirationDate to public;
 
-create table employee(
+create table employee( 
+	-- ERROR: ORA-00955: name is already used by an existing object
+	username varchar2(30) not null,						
 	phone number,
 	name varchar2(30),
-	username varchar2(30),
 	password varchar2(30),
 	primary key (username)
 );
 
 grant select on employee to public;
 
-create table expiresOn(
-	id varchar2(255),
+create table expiresOn( 						
+	id varchar2(255) not null,
 	name varchar2(30),
-	exdate date,
+	exdate number,
 	primary key (id)
 );
 
 grant select on expiresOn to public;
 
-create table money_collect(
-	did varchar2(255),
-	name varchar2(30),
-	phone number,
+create table money_collects( 					
+	did varchar2(255) not null,
+	dname varchar2(30),
+	dphone number,
 	moneydate date not null,
-	username varchar2 not null,
-	amount decimal(10,2),
-	medium varchar2(30),
+	username varchar2(30) not null, 
+-- ERROR ; ORA-00906: missing left parenthesis 
+	amount decimal(10,2) not null,
+	medium varchar2(30) not null,
 	primary key (did),
 	foreign key (username) references employee (username) ON DELETE CASCADE ON UPDATE CASCADE
 	);
 
 grant select on money_collects to public;
+	-- EROOR: MONEY_COLLECTS DOESNT EXIST
 
 create table item_collects(
-	did varchar2(30),
+	did varchar2(255) not null,
 	name varchar2(30),
 	phone number,
 	itemdate date,
@@ -75,68 +91,61 @@ create table item_collects(
 	item varchar2(30) not null,
 	primary key (did),
 	foreign key (username) references employee (username) ON UPDATE CASCADE
+	--ERROR: RA-00905: missing keyword
+
+
 );
 
-grant select on item_collects to public;
-
--- TODO: KIKI
--- create table item_distributes(
--- 	string varchar2(255),
--- 	category varchar2(255),
--- 	location varchar2(255),
--- 	startTime time,
--- 	length float(53)
--- 	letter char(1),
--- 	primary key (string)
--- 	foreign key(startTime,length,letter) references distribution_shift (startTime,length,letter) ON UPDATE CASCADE
--- );
---
--- grant select on item_distributes to public;
+grant select on item_collects to public; -- ERORR; TABLE DOESNT EXIST
 
 create table food_g (
-	value varchar2(30) primary key
+	-- ERORR: ORA-00955: name is already used by an existing object
+	value varchar2(30) not null primary key
 );
 
 grant select on food_g to public;
 
 create table f_has (
-	value varchar2(30),
-	name varchar2(30),
+	value varchar2(30) not null,
+	name varchar2(30) not null,
 	primary key(value,name),
 	foreign key(value) references food_g (value),
-	foreign key(name) references item (name)
+	foreign key(name) references item_Table (name)
 );
 
 grant select on f_has to public;
 
 create table collection_work (
-	startTime number,
-	length decimal(5,2),
-	letter varchar2(4),
-	sdate date,
+	startTime number not null,
+	length decimal(5,2) not null,
+	letter varchar2(4) not null,
+	sdate date not null,
 	username varchar2(30) not null,
 	primary key (startTime,length,letter, sdate),
 	foreign key(startTime,length,letter,sdate) references shift (startTime,length,letter,sdate) ON DELETE CASCADE ON UPDATE CASCADE,
+	-- ERROR: on update 00907: missing right parenthesis
 	foreign key (username) references employee (username) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 grant select on collection_work to public;
+	-- EROOR: DNE
 
 create table distribution_work (
-	startTime number,
-	length decimal(5,2),
-	letter char(4),
-	sdate date,
-	username varchar2(30),
+	startTime number not null,
+	length decimal(5,2) not null,
+	letter char(4) not null,
+	sdate date not null,
+	username varchar2(30) not null,
 	primary key(startTime,length,letter,sdate),
 	foreign key(startTime,length,letter,sdate) references shift (startTime,length,letter,sdate) ON DELETE CASCADE,
+	-- error table dne
 	foreign key (username) references employee (username) ON DELETE CASCADE
 );
 
-grant select on distribution_work to public;
+grant select on distribution_work to public; -- ERORR; DNE
 
-create table purchase_make (
-	pid varchar2(255) primary key,
+create table purchase_makes(
+	pid varchar2(255) not null primary key,
 	pamount decimal(10,2) not null,
 	userName varchar2(30) not null,
 	item varchar2(30) not null,
@@ -145,10 +154,10 @@ create table purchase_make (
 
 grant select on purchase_make to public;
 
-create table shift (
+create table shift (  						
 	startTime number not null,
-	length decimal(5,2),
-	letter varchar2(4),
+	length decimal(5,2) not null,
+	letter varchar2(4) not null,
 	sdate date not null,
 	primary key(startTime,length,letter,sdate)
 );
@@ -156,153 +165,139 @@ create table shift (
 grant select on shift to public;
 
 create table volunteer_add(
-	v_uname varchar2(30) primary key
-	userName varchar2(30),
+	v_uname varchar2(30) not null primary key
+	userName varchar2(30) not null,
+	-- erorr; missing right parenthesis
 	foreign key(v_uname)references employee (userName) ON DELETE CASCADE,
 	foreign key(userName) references admin (userName) ON DELETE CASCADE
 );
 
---grant select on volunteer_add to public;
+grant select on volunteer_add to public;
 
 create table admin (
-	userName varchar2(30),
+	-- ERORR; ORA-00955: name is already used by an existing object
+	userName varchar2(30) not null,
 	primary key(userName),
 	foreign key (userName) references employee (userName) ON DELETE CASCADE
 );
 
 grant select on admin to public;
 
--- INSERT money_collects data
+-- INSERT item_Table data
+-- ERROR: Location column not allowed there??
+insert into item_Table values("Bread", "Food",  "PantryC");
+insert into item_Table values("Linen", "Other", "PantryB");
+insert into item_Table values("Vegetable Oil", "Food", "PantryA");
+insert into item_Table values("Pasta", "Food", "PantryA");
+insert into item_Table values("Canned Beans", "Food", "PantryA");
 
-insert into money_collects values(1,"John Smith", 6041234567,2018-05-31, 100.25, "credit", 10:45:00, 1.25, "A");
-insert into money_collects values(2,"Jane Doe", 6048463847,2018-05-31, 125.25, "cash", 12:45:00, 2.50, "A");
-insert into money_collects values(4,"Chad Smith", 7788297482,2018-12-03, 110.25, "credit", 13:00:00, 1.00, "A");
-insert into money_collects values(6,"Sarah Lee", 8391848294,2015-01-13, 200.00, "cash", 15:00:00, 2.00, "A");
-insert into money_collects values(7,"Amber Chan", 7784920412,2000-07-23, 135.00, "cash", 14:30:00, 2.25, "A");
-
--- INSERT item_collects data
-insert into item_collects values(3,"John Doe", 6048593123,2018-02-10, 10:45:00, 1.15, "A");
-insert into item_collects values(5,"Jessica Hope", 77892847293,2018-02-10, 12:45:00, 3.00, "A");
-insert into item_collects values(8,"Jack Ko", 2048573859,2017-08-11, 13:00:00, 2.15, "A");
-insert into item_collects values(9,"Liz Apple", 4739174824,2018-05-12, 14:30:00, 2.00, "A");
-insert into item_collects values(10,"Nick Brown ", 6045829104,2018-11-10, 15:00:00, 1.45, "A");
-
--- INSERT item_is data
-insert into item_is values("Bread", 3);
-insert into item_is values("Canned Pineapple", 5);
-insert into item_is values("Vegetable Oil", 8);
-insert into item_is values("Pasta", 9);
-insert into item_is values("Canned Beans", 10);
-
--- INSERT item_distributes data
-insert into item_distributes values("Bread", "Food","PantryC", 07:55:00, 00.50, "A");
-insert into item_distributes values("Linen", "Non-Food","PantryB", 09:25:00, 00.25, "A");
-insert into item_distributes values("Vegetable Oil", "Food","PantryA", 12:45:00, 00.15, "A");
-insert into item_distributes values("Pasta", "Food","PantryA", 13:35:00, 00.10, "A");
-insert into item_distributes values("Canned Beans", "Food","PantryA", 14:15:00, 00.15, "A");
-
--- INSERT group data
-insert into group values("Carbohydrates");
-insert into group values("Non-nutritional");
-insert into group values("Protein");
-insert into group values("Fats");
-insert into group values("Produce");
-
--- INSERT has data
-insert into has values("Bread", "Carbohydrates");
-insert into has values("Linen", );
-insert into has values("Vegetable Oil", "Fats");
-insert into has values("Pasta", "Carbohydrates");
-insert into has values("Canned Beans", "Protein");
-
--- INSERT collection data
-insert into collection values(10:45:00, 1.25, "A");
-insert into collection values(12:45:00, 2.50, "A");
-insert into collection values(13:00:00, 1.00, "A");
-insert into collection values(15:00:00, 2.00, "A");
-insert into collection values(14:30:00, 2.15, "A");
-
--- INSERT distribution data
-insert into distribution values(10:45:00, 1.25, "A");
-insert into distribution values(12:45:00, 2.50, "A");
-insert into distribution values(13:00:00, 1.00, "A");
-insert into distribution values(15:00:00, 2.00, "A");
-insert into distribution values(14:30:00, 2.15, "A");
-
--- INSERT adds data
-insert into adds values("Bread", 1);
-insert into adds values("Linen", 2);
-insert into adds values("Canned Soup", 3);
-insert into adds values("Tampons", 4);
-insert into adds values("Pasta", 5);
-
--- INSERT purchase_makes data
-insert into purchase_makes values(1,34.23, "admin0", "Bread");
-insert into purchase_makes values(2,23.56, "admin3", "Linen");
-insert into purchase_makes values(3,30.01, "admin4", "Canned Soup");
-insert into purchase_makes values(4,65.98, "admin0", "Tampons");
-insert into purchase_makes values(5,77.02, "admin0", "Pasta");
-
--- INSERT shift_logdelete data
-insert into shift_logDelete values("admin0", 10:45:00, 1.25, "A");
-insert into shift_logDelete values("admin0", 12:45:00, 2.50, "A");
-insert into shift_logDelete values("admin3", 08:45:00, 1.25, "A");
-insert into shift_logDelete values("admin1", 10:15:00, 3.50, "A");
-insert into shift_logDelete values("admin1", 13:45:00, 4.00, "A");
-
--- INSERT works data
-insert into works values (10:45:00, 1.25, "A", "volunteer0");
-insert into works values (12:45:00, 2.50, "A", "volunteer1");
-insert into works values (08:45:00, 1.25, "A", "volunteer2");
-insert into works values (10:15:00, 3.50, "A", "volunteer3");
-insert into works values (13:45:00, 4.00, "A", "volunteer4");
-
--- INSERT volunteer_add data
-insert into volunteer_add values ("volunteer1", "admin0");
-insert into volunteer_add values ("volunteer2", "admin1");
-insert into volunteer_add values ("volunteer3", "admin1");
-insert into volunteer_add values ("volunteer4", "admin0");
-insert into volunteer_add values ("volunteer5", "admin1");
-
--- INSERT administrator data
-insert into administrator values ("admin0");
-insert into administrator values ("admin1");
-insert into administrator values ("admin2");
-insert into administrator values ("admin3");
-insert into administrator values ("admin4");
+-- INSERT expirationDate data
+-- eroor EXPECT DATE GOT NUMBER??? but we want number
+insert into expirationDate values(20190412);
+insert into expirationDate values(19830129);
+insert into expirationDate values(20200304);
+insert into expirationDate values(2004044);
+insert into expirationDate values(20231207);
 
 -- INSERT Employee data
+-- PASSWORD NO COLUMN 
 insert into employee values ("admim0", 6045939281, "Test User", "Password");
+insert into employee values ("admim1", 6045939281, "Test User", "Password");
+insert into employee values ("admim2", 6045939281, "Test User", "Password");
 insert into employee values ("volunteer1", 6048392860, "Test Volun1", "PAssword");
 insert into employee values ("volunteer2", 6048271843, "Test Volun2", "PaSsword");
 insert into employee values ("volunteer3", 6049402281, "Test Volun3", "PasSword");
 insert into employee values ("volunteer4", 6041111111, "Test Volun4", "PassWord");
 
 -- INSERT expireson data
-insert into expireson values("Bread", TO_DATE('2019-04-12', 'YYYY-MM-DD'));
-insert into expireson values("Linen", TO_DATE('1983-01-29', 'YYYY-MM-DD'));
-insert into expireson values("Vegetable Oil", TO_DATE('2020-03-04', 'YYYY-MM-DD'));
-insert into expireson values("Pasta", TO_DATE('2004-04-4', 'YYYY-MM-DD'));
-insert into expireson values("Canned Beans", TO_DATE('2023-12-07', 'YYYY-MM-DD'));
+-- ERORR BREAD COLUMN DNE
+insert into expireson values("1", "Bread", 20190412);
+insert into expireson values("2","Linen", 19830129);
+insert into expireson values("3","Vegetable Oil",20200304);
+insert into expireson values("4","Pasta", 2004044);
+insert into expireson values("5","Canned Beans",20231207);
 
--- INSERT expirationDate data
-insert into expirationDate values(TO_DATE('2019-04-12', 'YYYY-MM-DD'));
-insert into expirationDate values(TO_DATE('1983-01-29', 'YYYY-MM-DD'));
-insert into expirationDate values(TO_DATE('2020-03-04', 'YYYY-MM-DD'));
-insert into expirationDate values(TO_DATE('2004-04-4', 'YYYY-MM-DD'));
-insert into expirationDate values(TO_DATE('2023-12-07', 'YYYY-MM-DD'));
+-- INSERT money_collects data
+-- ERROR TABLE DNE
+insert into money_collects values("1","John Smith", 6041234567,2018-05-31,"volunteer1", 100.25, "credit");
+insert into money_collects values("2","Jane Doe", 6048463847,2018-05-31,"volunteer4", 125.25, "cash");
+insert into money_collects values("4","Chad Smith", 7788297482,2018-12-03,"volunteer2", 110.25, "credit");
+insert into money_collects values("6","Sarah Lee", 8391848294,2015-01-13,"volunteer3", 200.00, "cash");
+insert into money_collects values("7","Amber Chan", 7784920412,2000-07-23,"volunteer4",135.00, "cash");
 
--- INSERT shfit data
-insert into shift values(10:45:00, 1.25, "A");
-insert into shift values(12:45:00, 2.50, "A");
-insert into shift values(13:00:00, 1.00, "A");
-insert into shift values(15:00:00, 2.00, "A");
-insert into shift values(14:30:00, 2.15, "A");
+-- INSERT item_collects data
+-- ERROR TABLE DNE
+insert into item_collects values("3","John Doe", 6048593123,2018-01-01,"volunteer1","Bread");
+insert into item_collects values("5","Jessica Hope", 77892847293,2018-02-10,"volunteer1","Canned Pineapple");
+insert into item_collects values("8","Jack Ko", 2048573859,2017-08-11,"volunteer2","Vegetable Oil");
+insert into item_collects values("9","Liz Apple", 4739174824,2018-05-12, "volunteer3","Pasta");
+insert into item_collects values("10","Nick Brown ", 6045829104,2018-11-10,"volunteer3","Canned Beans");
+
+-- INSERT food_g data
+-- ERROR column DNE 
+insert into food_g values("Carbohydrates");
+insert into food_g values("Other");
+insert into food_g values("Vegetable Oil");
+insert into food_g values("Carbohydrates");
+insert into food_g values("Protein");
+
+-- INSERT f_has data 
+-- ERROR column BREAD dne
+insert into f_has values("Carbohydrates","Bread");
+insert into f_has values("Other", "Linen" );
+insert into f_has values("Fats","Vegetable Oil" );
+insert into f_has values("Carbohydrates","Pasta");
+insert into f_has values("Protein","Canned Beans");
 
 
--- INSERT item data
-insert into item values("Bread", "Food", TO_DATE('2019-04-12', 'YYYY-MM-DD'), "PantryC");
-insert into item values("Linen", "Other", TO_DATE('1983-01-29', 'YYYY-MM-DD'), "PantryB");
-insert into item values("Vegetable Oil", "Food", TO_DATE('2020-03-04', 'YYYY-MM-DD'), "PantryA");
-insert into item values("Pasta", "Food", TO_DATE('2004-04-4', 'YYYY-MM-DD'), "PantryA");
-insert into item values("Canned Beans", "Food", TO_DATE('2023-12-07', 'YYYY-MM-DD'), "PantryA");
+-- INSERT collection_work data
+-- ERROR TABLE DNE
+insert into collection_work values(1045, 1.25, "A", 2018-01-01, "volunteer1");
+insert into collection_work values(1245, 2.50, "A",2018-02-10, "volunteer1");
+insert into collection_work values(1300, 1.00, "A",2017-08-11,"volunteer2");
+insert into collection_work values(1500, 2.00, "A",2018-05-12,"volunteer3");
+insert into collection_work values(1430, 2.15, "A",2018-11-10,"volunteer4");
+
+-- INSERT distribution_work data
+-- ERROR TABLE DNE
+insert into distribution_work values(2045, 1.25, "B", 2018-01-01, "volunteer1");
+insert into distribution_work values(2045, 2.50, "C",2018-02-10, "volunteer1");
+insert into distribution_work values(2000, 1.00, "D",2017-08-11,"volunteer2");
+insert into distribution_work values(2000, 2.00, "E",2018-05-12,"volunteer3");
+insert into distribution_work values(2030, 2.15, "F",2018-11-10,"volunteer4");
+
+
+-- INSERT purchase_makes data
+-- ERROR BREAD COLUMN DNE
+insert into purchase_makes values("1",34.23, "admin0", "Bread");
+insert into purchase_makes values("2",23.56, "admin1", "Linen");
+insert into purchase_makes values("3",30.01, "admin2", "Canned Soup");
+insert into purchase_makes values("4",65.98, "admin0", "Tampons");
+insert into purchase_makes values("5",77.02, "admin0", "Pasta");
+
+-- INSERT shift data
+-- ERROR letter COLUMN DNE
+insert into shift values(1045, 1.25, "A",2018-01-01);
+insert into shift values(1245, 2.50, "A",2018-02-10);
+insert into shift values(1300, 1.00, "A",2017-08-11);
+insert into shift values(1500, 2.00, "A",2018-05-12);
+insert into shift values(1430, 2.15, "A",2018-11-10);
+-- distribution shift
+insert into shift values(2045, 1.25, "B", 2018-01-01);
+insert into shift values(2045, 2.50, "C",2018-02-10);
+insert into shift values(2000, 1.00, "D",2017-08-11);
+insert into shift values(2000, 2.00, "E",2018-05-12);
+insert into shift values(2030, 2.15, "F",2018-11-10);
+
+-- INSERT volunteer_add data
+-- ERROR TABLE DNE
+insert into volunteer_add values ("volunteer1", "admin0");
+insert into volunteer_add values ("volunteer2", "admin1");
+insert into volunteer_add values ("volunteer3", "admin2");
+insert into volunteer_add values ("volunteer4", "admin0");
+
+-- INSERT admin data
+-- ERROR COLUMN DNE
+insert into admin values ("admin0");
+insert into admin values ("admin1");
+insert into admin values ("admin2");
